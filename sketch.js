6e324cb6e3a297996516df811a0a09a0877c18a4
mkiller4040 <swagata.deb@gmail.com>
2020-07-23 20:44:28 -0700

    changed background according to time

--- sketch.js
+++ sketch.js
@@ -10,15 +10,15 @@ var bird, slingshot;
 
 var gameState = "onSling";
 
-function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+function preload() 
+{
+  getBackgroundImg();
 }
 
 function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
@@ -45,44 +45,71 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
-    Engine.update(engine);
-    //strokeWeight(4);
-    box1.display();
-    box2.display();
-    ground.display();
-    pig1.display();
-    log1.display();
-
-    box3.display();
-    box4.display();
-    pig3.display();
-    log3.display();
-
-    box5.display();
-    log4.display();
-    log5.display();
-
-    bird.display();
-    platform.display();
-    //log6.display();
-    slingshot.display();    
+    if(backgroundImg)
+    {
+        background(backgroundImg);
+        Engine.update(engine);
+        //strokeWeight(4);
+        box1.display();
+        box2.display();
+        ground.display();
+        pig1.display();
+        log1.display();
+    
+        box3.display();
+        box4.display();
+        pig3.display();
+        log3.display();
+    
+        box5.display();
+        log4.display();
+        log5.display();
+    
+        bird.display();
+        platform.display();
+        //log6.display();
+        slingshot.display();    
+    }
 }
 
-function mouseDragged(){
-    if (gameState!=="launched"){
+function mouseDragged()
+{
+    if (gameState!=="launched")
+    {
         Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
     }
 }
 
 
-function mouseReleased(){
+function mouseReleased()
+{
     slingshot.fly();
     gameState = "launched";
 }
 
-function keyPressed(){
-    if(keyCode === 32){
+function keyPressed()
+{
+    if(keyCode === 32)
+    {
        // slingshot.attach(bird.body);
     }
+}
+
+ async function getBackgroundImg()
+{
+  var timeZoneinfo = await fetch("http://worldclockapi.com/api/json/cst/now");
+  var timeZoneinfoJSON = await timeZoneinfo.json();
+  var datetime = timeZoneinfoJSON.currentDateTime;
+  var hour = datetime.slice(11,13);
+
+  if(hour >= 05 && hour <= 19)
+  {
+    bg = "sprites/bg.png"
+  } 
+  else 
+  {
+    bg = "sprites/bg2.jpg"
+  }
+
+  backgroundImg = loadImage(bg);
 }
